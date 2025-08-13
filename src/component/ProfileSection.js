42943477718df27640b020f1f4a9e447909921d4
Ganjar <emailmganjar@gmail.com>
2025-08-13 14:42:01 +0700

    update resume

--- src/component/ProfileSection.js
+++ src/component/ProfileSection.js
@@ -75,7 +75,7 @@ export default function ProfileSection({ resumeData }) {
           <Button
             variant="contained"
             sx={{ m: 1, textTransform: 'capitalize' }}
-            href="/[Resumev2]%20Muhammad%20Ganjar%20Imanudin.pdf"
+            href="/Resume%20-%20Muhammad%20Ganjar%20Imanudin.pdf"
             target="_blank"
           >
             My Resume
